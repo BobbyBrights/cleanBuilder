3a76b073c38f17309f60851f2a616bdad1d36fc9
Jacen <jason.ho@bibsolution.com>
2019-03-07 14:27:40 +0800

    Version 1.0

---
+++ src/js/chunk-index.js
@@ -0,0 +1,63 @@
+import $ from "jquery";
+// import  {TweenMax}  from "gsap/TweenMax";
+// import ScrollMagic from 'scrollmagic';
+// import 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+// import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+
+
+
+
+export default function indexInit () {
+ 
+    alert("INDEX JS");
+    // scrollAnimation();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+  function scrollAnimation() {
+    
+	// init controller
+	var controller = new ScrollMagic.Controller();
+
+	// build tween
+	var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 200, triggerHook: "onLeave"})
+					.setTween(tween)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+
+	// change behaviour of controller to animate scroll instead of jump
+	controller.scrollTo(function (newpos) {
+		TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+	});
+
+	//  bind scroll to anchor links
+	$(document).on("click", "a[href^='#']", function (e) {
+		var id = $(this).attr("href");
+		if ($(id).length > 0) {
+			e.preventDefault();
+
+			// trigger scroll
+			controller.scrollTo(id);
+
+				// if supported by the browser we can even update the URL.
+			if (window.history && window.history.pushState) {
+				history.pushState("", document.title, id);
+			}
+		}
+	});
+  }
